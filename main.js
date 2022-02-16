86d9e349c1c3d5eb4905d454cf490770bd0e5372
MAvi-Rup <avirupmondal19@gmail.com>
2022-02-17 03:16:05 +0600

    Balance Functionality work Done

--- main.js
+++ main.js
@@ -1,1 +1,25 @@
-console.log('connected')
+function getInputField(id){
+    const input = document.getElementById(id+'-input')
+    const inputValue = parseFloat(input.value)
+    return inputValue
+}
+
+document.getElementById('calculate-total').addEventListener('click',(e)=>{
+    const inputIncome = getInputField('income')
+    const foodTotal = getInputField('food')
+    const rentTotal = getInputField('rent')
+    const clothesTotal = getInputField('clothes')
+    const totalExpense = document.getElementById('expenses-total')
+    const totalBalance = document.getElementById('balance-total')
+   if(foodTotal>0 && rentTotal>0 && clothesTotal>0){
+       const tottal= foodTotal+rentTotal+clothesTotal
+       if(tottal<inputIncome){
+           totalExpense.innerText = tottal;
+           totalBalance.innerText = inputIncome-tottal
+       }else{
+           alert('Insufficient Balance in your Account')
+       }
+   }else{
+       alert('Expenses can not be negative value')
+   }
+})
